24dc885db4f765d4e5785243db0fa340ef996b63
Al-Amin Firdows <alaminfirdows@gmail.com>
2025-10-17 01:33:25 +0600

    Bismillah! (initial commit)

---
+++ src/LaravelOpaqueIdServiceProvider.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AlAminFirdows\LaravelOpaqueId;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelOpaqueIdServiceProvider extends ServiceProvider
+{
+}
